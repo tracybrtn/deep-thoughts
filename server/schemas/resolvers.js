4ea66a4c8b924c6c792998748c5d97117761211c
tracybrtn <tracynburton@gmail.com>
2022-12-13 11:32:07 -0600

    basic authorization functional

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,22 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // me method
+    me: async (parent, args, context) => {
+      if(context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+
+        return userData;
+      }
+      
+      throw new AuthenticationError('Not logged in');
+    },
     // get all users
     users: async () => {
       return User.find()
@@ -22,6 +37,32 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
+    }
+  },
+  Mutation: {
+    //adds new users
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    //logs in existing users
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user){
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     }
   }
 };
